65f13288b59cf9f81bd63daa88766be41100cba9
Anass Akera <50927517+anassakera@users.noreply.github.com>
2025-11-17 12:15:35 +0100

    commit_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:idea/home_screen.dart';
+import 'package:idea/dashboard_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: const DashboardScreen(),
     );
   }
 }
